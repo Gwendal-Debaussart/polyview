fbe07a7bd39b0c2af6f70c0ad7d985aee5f30021
Gwendal200 <gwendal.deb@gmail.com>
2026-03-31 15:37:41 +0200

    fix formatting issues

--- polyview/__init__.py
+++ polyview/__init__.py
@@ -3,57 +3,63 @@
 This module exposes the most commonly used classes and helpers at the
 package root to support a concise import style:
 
-	import polyview as pv
-	mvd = pv.MultiViewDataset([...])
-	model = pv.MultiviewKMeans(...)
+        import polyview as pv
+        mvd = pv.MultiViewDataset([...])
+        model = pv.MultiviewKMeans(...)
 """
 
 from importlib.metadata import PackageNotFoundError, version
 
 from . import cluster, dataset, fusion
 from .base import (
-	BaseMultiView,
-	BaseMultiViewClusterer,
-	BaseMultiViewEmbedder,
-	BaseMultiViewTransformer,
+    BaseMultiView,
+    BaseMultiViewClusterer,
+    BaseMultiViewEmbedder,
+    BaseMultiViewTransformer,
 )
-from .cluster.mv_kmeans import MultiviewKMeans
+from .cluster.mv_kmeans import (
+    MultiViewKMeans,
+)
+from .cluster.mv_coreg_sc import (
+    MultiViewCoRegSpectralClustering,
+)
 from .dataset.multiview import MultiViewDataset
 from .fusion.early import ConcatFusion, NormalizedFusion, WeightedFusion
 from .fusion.kernel_fusion import (
-	KernelFusion,
-	KernelSpec,
-	center_kernel,
-	is_valid_kernel,
-	normalize_kernel,
+    KernelFusion,
+    KernelSpec,
+    center_kernel,
+    is_valid_kernel,
+    normalize_kernel,
 )
 
 # Backward-compatible alias matching the README namespace.
 datasets = dataset
 
 try:
-	__version__ = version("polyview")
+    __version__ = version("polyview")
 except PackageNotFoundError:
-	__version__ = "0.0.0"
+    __version__ = "0.0.0"
 
 __all__ = [
-	"__version__",
-	"cluster",
-	"dataset",
-	"datasets",
-	"fusion",
-	"BaseMultiView",
-	"BaseMultiViewTransformer",
-	"BaseMultiViewClusterer",
-	"BaseMultiViewEmbedder",
-	"MultiViewDataset",
-	"MultiviewKMeans",
-	"ConcatFusion",
-	"WeightedFusion",
-	"NormalizedFusion",
-	"KernelSpec",
-	"KernelFusion",
-	"center_kernel",
-	"normalize_kernel",
-	"is_valid_kernel",
+    "__version__",
+    "cluster",
+    "dataset",
+    "datasets",
+    "fusion",
+    "BaseMultiView",
+    "BaseMultiViewTransformer",
+    "BaseMultiViewClusterer",
+    "BaseMultiViewEmbedder",
+    "MultiViewDataset",
+    "MultiViewKMeans",
+    "MultiViewCoRegSpectralClustering",
+    "ConcatFusion",
+    "WeightedFusion",
+    "NormalizedFusion",
+    "KernelSpec",
+    "KernelFusion",
+    "center_kernel",
+    "normalize_kernel",
+    "is_valid_kernel",
 ]
