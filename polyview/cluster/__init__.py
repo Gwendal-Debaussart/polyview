35e594c26b7ef56b3abacbd52e4bcc5f4925bbd5
Gwendal200 <gwendal.deb@gmail.com>
2026-04-15 17:06:45 +0200

    add: mv_nmf

--- polyview/cluster/__init__.py
+++ polyview/cluster/__init__.py
@@ -3,5 +3,6 @@
 from .mv_kmeans import MultiViewKMeans
 from .mv_coreg_sc import MultiViewCoRegSpectralClustering
 from .mv_cotrain_sc import MultiViewCoTrainSpectralClustering
+from .mv_nmf import MultiViewNMF
 
-__all__ = ["MultiViewKMeans", "MultiViewCoRegSpectralClustering", "MultiViewCoTrainSpectralClustering"]
+__all__ = ["MultiViewKMeans", "MultiViewCoRegSpectralClustering", "MultiViewCoTrainSpectralClustering", "MultiViewNMF"]
